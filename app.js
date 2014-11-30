ac4ba506a1ac8ea39f693f4231b8eddfc387f6a9
Zach Latta <zach@zachlatta.com>
2014-11-30 14:02:48 -0800

    Install snapchat library and set up login.

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 var express = require("express");
+var snapchat = require("snapchat");
+var fs = require("fs");
+
+var client = new snapchat.Client();
 var app = express();
+
+var SNAPCHAT_USERNAME = 'TODO';
+var SNAPCHAT_PASSWORD = 'TODO';
+
+function createDirIfNotExists(name) {
+  if (!fs.existsSync(name)) {
+    fs.mkdirSync(name);
+  }
+}
 
 app.get('/', function (req, res) {
   res.sendFile('index.html', {root: __dirname});
 })
 
-var server = app.listen(1759, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+client.login(SNAPCHAT_USERNAME, SNAPCHAT_PASSWORD)
+  .then(function (data) {
+    console.log("Logged into Snapchat as %s", SNAPCHAT_USERNAME);
 
-  console.log("Snaplive listening on http://%s:%s", host, port);
-});
+    createDirIfNotExists('./images');
+
+    var server = app.listen(1759, function () {
+      var host = server.address().address;
+      var port = server.address().port;
+
+      console.log("Snaplive listening on http://%s:%s", host, port);
+    });
+  }, function (err) {
+    console.log(err);
+  });
