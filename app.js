904c935aa98333c442eefb905e1703507e173fe2
Zach Latta <zach@zachlatta.com>
2014-12-01 15:35:23 -0800

    Download received snaps.

--- app.js
+++ app.js
@@ -14,6 +14,42 @@ function createDirIfNotExists(name) {
   }
 }
 
+function downloadUnreadSnaps(client, snaps) {
+  var update = {}; // update we'll send to snapchat after downloading all snaps
+
+  snaps.forEach(function (snap) {
+    if (typeof snap.sn !== 'undefined' && typeof snap.t !== 'undefined' &&
+        snap.st == 1) {
+      console.log('Saving snap from ' + snap.sn + '...');
+
+      // save the image to ./images/{SENDER USERNAME}_{SNAP ID}.jpg
+      var stream =
+        fs.createWriteStream('./images/' + snap.sn + '_' + snap.id + '.jpg', {
+          flags: 'w',
+          encoding: null,
+          mode: 0666
+        });
+      client.getBlob(snap.id).then(function(blob) {
+        blob.pipe(stream);
+        blob.resume();
+      });
+
+      // add the snap to the update we'll send to snapchat, telling it to mark
+      // the snap as read
+      update[snap.id] = {
+        c: 0, // mark snap as seen
+        t: (new Date).getTime(), // set timestamp to the epoch
+        replayed: 0 // we have not replayed the snap
+      };
+    }
+  });
+
+  // send our update to snapchat, marking all snaps we just downloaded as read
+  client.sync(update);
+}
+
+app.use('/images', express.static(__dirname + '/images'));
+
 app.get('/', function (req, res) {
   res.sendFile('index.html', {root: __dirname});
 })
@@ -24,6 +60,19 @@ client.login(SNAPCHAT_USERNAME, SNAPCHAT_PASSWORD)
 
     createDirIfNotExists('./images');
 
+    // make sure we allow snaps from any account
+    client.privacy(false);
+
+    // run the actions in the passed function every 5 seconds
+    setInterval(function () {
+      // get the latest updates from snapchat
+      client.sync().then(function (data) {
+        // download the unread snaps from the update
+        downloadUnreadSnaps(client, data.snaps);
+      });
+    }, 5000);
+
+    // start our webserver on port 1759
     var server = app.listen(1759, function () {
       var host = server.address().address;
       var port = server.address().port;
